2698b26dbfdba98ecfb428a81106748dd546e1b2
Anna <evlanova-a@mail.ru>
2025-07-02 21:53:45 +0300

    DZ301

---
+++ src/test/java/database/CRUDUtils.java
@@ -0,0 +1,205 @@
+package database;
+
+import database.jdbc.DatabaseConnection;
+import lombok.SneakyThrows;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class CRUDUtils {
+    private static final String insertAnimal = "INSERT INTO public.animal (id, \"name\", age, \"type\", sex, place) VALUES(?, ?, ?, ?, ?, ?);\n";
+    private static final String insertWorkman = "INSERT INTO public.workman (id, \"name\", age, \"position\") VALUES(1, null, 23, 1);\n";
+
+    private static final String insertPlaces = "INSERT INTO public.places (id, \"row\", place_num, \"name\") VALUES(?, ?, ?, ?);\n";
+
+    private static final String getCountAnimalRow = "SELECT count(*) from animal";
+
+    private static final String getCountPlacesRow = "SELECT count(*) from places";
+
+
+    public static List<Animal> getAnimalData(String query) {
+        List<Animal> animals = new ArrayList<>();
+        try {
+            Connection connection = DatabaseConnection.createConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String name = resultSet.getString("name");
+                int age = resultSet.getInt("age");
+                int type = resultSet.getInt("type");
+                int sex = resultSet.getInt("sex");
+                int place = resultSet.getInt("place");
+
+                animals.add(new Animal(id, name, age, type, sex, place));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return animals;
+    }
+
+    @SneakyThrows
+    public static void insertAnimalData(Animal animal) {
+        Connection connection = DatabaseConnection.createConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(insertAnimal);
+        preparedStatement.setInt(1, animal.getId());
+        preparedStatement.setString(2, animal.getName());
+        preparedStatement.setInt(3, animal.getAge());
+        preparedStatement.setInt(4, animal.getType());
+        preparedStatement.setInt(5, animal.getSex());
+        preparedStatement.setInt(6, animal.getPlace());
+        preparedStatement.executeUpdate();
+    }
+
+    public static boolean insertWorkmanData() {
+        try {
+            Connection connection = DatabaseConnection.createConnection();
+            Statement statement = connection.createStatement();
+
+            statement.executeUpdate(insertWorkman);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return false;
+    }
+
+    public static boolean insertPlacesData(Places place){
+        try {
+            Connection connection = DatabaseConnection.createConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(insertPlaces);
+            preparedStatement.setInt(1, place.getId());
+            preparedStatement.setInt(2, place.getRow());
+            preparedStatement.setInt(3, place.getPlace_num());
+            preparedStatement.setString(4, place.getName());
+            preparedStatement.executeUpdate();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return true;
+    }
+
+    public static int getCountRowByTable(String tableName) throws SQLException {
+        Connection connection = DatabaseConnection.createConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT count (*) from " + tableName);
+        resultSet.next();
+        int count = resultSet.getInt("count(*)");
+        System.out.printf("В таблице public.%s ровно %s записей%n", tableName, count);
+        return count;
+    }
+
+    public static int getZooCountRow() throws SQLException {
+        Connection connection = DatabaseConnection.createConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT count (*) from zoo");
+        resultSet.next();
+        int count = resultSet.getInt("count(*)");
+        System.out.println(resultSet.getInt("count(*)"));
+        return count;
+    }
+
+    @SneakyThrows
+    public static int getAnimalCountRow() {
+        Connection connection = DatabaseConnection.createConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery(getCountAnimalRow);
+        resultSet.next();
+        return resultSet.getInt("count(*)");
+    }
+
+    public static int getRowCountByTable(String tableName) throws SQLException {
+        Connection connection = DatabaseConnection.createConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT count (*) from " + tableName);
+        resultSet.next();
+        int count = resultSet.getInt("count(*)");
+        System.out.printf("Table public.%s has exact %s rows%n", tableName, count);
+        return count;
+    }
+
+    public static int getPlacesCountRow() throws SQLException {
+        Connection connection = DatabaseConnection.createConnection();
+        Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT count (*) from places");
+        resultSet.next();
+        int count = resultSet.getInt("count(*)");
+        System.out.println(resultSet.getInt("count(*)"));
+        return count;
+    }
+
+    public static List<String> getZooNameData() {
+        String name;
+        List<String> zoo = new ArrayList<>();
+        try {
+            Connection connection = DatabaseConnection.createConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement("SELECT id,  \"name\" from public.zoo");
+            ResultSet resultSet = preparedStatement.executeQuery();
+
+            while (resultSet.next()) {
+                name = resultSet.getString("name");
+                System.out.println(name);
+                zoo.add(name);
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+        return zoo;
+    }
+    public static Animal getAnimalById(int id) throws SQLException {
+        String query = "SELECT * FROM animal WHERE id = ?";
+        try (Connection connection = DatabaseConnection.createConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            ResultSet resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                Animal animal = new Animal();
+                animal.setId(resultSet.getInt("id"));
+                animal.setName(resultSet.getString("name"));
+                animal.setAge(resultSet.getInt("age"));
+                animal.setType(resultSet.getInt("type"));
+                animal.setSex(resultSet.getInt("sex"));
+                animal.setPlace(resultSet.getInt("place"));
+                return animal;
+            }
+            return null;
+        }
+    }
+
+    /**
+     * Обновление данных животного
+     */
+    public static boolean updateAnimalData(Animal animal) throws SQLException {
+        String query = "UPDATE animal SET name = ?, age = ?, type = ?, sex = ?, place = ? WHERE id = ?";
+        try (Connection connection = DatabaseConnection.createConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, animal.getName());
+            statement.setInt(2, animal.getAge());
+            statement.setInt(3, animal.getType());
+            statement.setInt(4, animal.getSex());
+            statement.setInt(5, animal.getPlace());
+            statement.setInt(6, animal.getId());
+
+            return statement.executeUpdate() > 0;
+        }
+    }
+
+    /**
+     * Удаление животного по ID
+     */
+    public static boolean deleteAnimal(int id) throws SQLException {
+        String query = "DELETE FROM animal WHERE id = ?";
+        try (Connection connection = DatabaseConnection.createConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setInt(1, id);
+            return statement.executeUpdate() > 0;
+        }
+    }
+}
